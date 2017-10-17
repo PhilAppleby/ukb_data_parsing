0c56eba5da4078d11183505fb9812d312b70f7bc
Philip Appleby <pda11181@uk1salx00235.corpnet2.com>
2017-10-17 16:51:11 +0100

    remove sh dir

--- sh/bnf_words.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export DATADIR=${PROJROOT}/ukb/data
-export PYDIR=${PROJROOT}/ukb/py
-
-cat ${DATADIR}/medication_coding.tsv | python ${PYDIR}/bnf_words.py --bnffile=${DATADIR}/bnf_data_formatted.csv
--- sh/extract_medications_columns.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export DATADIR=${PROJROOT}/data/ukb/pheno
-export PYDIR=${PROJROOT}/devel/HIC.Genomics/ukb/py
-
-python ${PYDIR}/cut_main_csv_file.py --csvfile=${DATADIR}/ukb7002.csv --colprefs=137,20027,20003 > ${DATADIR}/medications_data.csv
